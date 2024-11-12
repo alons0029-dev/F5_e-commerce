c4d9fcfe5655009556dccd08e4bb7743abc3b822
alons0029-dev <alons0029.dev@gmail.com>
2024-11-12 11:08:46 +0100

    FIXED

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
-
-// REACT Components
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-// REACT-ROUTER-DOM Components
-import { RouterProvider } from 'react-router-dom';
+import './index.css';
+import { RouterProvider } from "react-router-dom";
 import { router } from './router';
 
-// Style
-import "./index.css";
-
-/* FUNCTIONALITY */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
